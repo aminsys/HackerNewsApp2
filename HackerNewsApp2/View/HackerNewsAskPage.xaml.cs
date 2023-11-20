f38d4a6f292f1143a7395a2c6fffa82afcac0056
Amin <amin.yassin@live.com>
2023-11-20 22:31:05 +0100

    Alpha version of infinite scrolling

--- HackerNewsApp2/View/HackerNewsAskPage.xaml.cs
+++ HackerNewsApp2/View/HackerNewsAskPage.xaml.cs
@@ -1,5 +1,6 @@
 using HackerNewsApp2.Model;
 using HtmlAgilityPack;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Http.Json;
 
@@ -10,6 +11,9 @@ public partial class HackerNewsAskPage : ContentPage
 
     private const string AskStories = "https://hacker-news.firebaseio.com/v0/askstories.json";
     private const string ApiUrl = "https://hacker-news.firebaseio.com/v0/item/";
+    private int postCounter = 0;
+    private List<string> askItemsTrimmed;
+    private ObservableCollection<HackerNewsPostModel> newsItems;
 
     public HackerNewsAskPage()
 	{
@@ -19,34 +23,34 @@ public partial class HackerNewsAskPage : ContentPage
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+        
+        await GetPostItems();
+        postCounter = 0;
 
-        List<HackerNewsPostModel> newsItems = await FetchNewsAsync();
+        newsItems = await FetchNewsAsync();
         NewsCollectionView.ItemsSource = newsItems;
-        loadingIndicator.IsRunning = false;
-        loadingIndicator.IsVisible = false;
+        //loadingIndicator.IsRunning = false;
+        //loadingIndicator.IsVisible = false;
     }
 
-    private async void OnFetchNewsClicked(object sender, EventArgs e)
+    /*private async void OnFetchMoreNewsClicked(object sender, EventArgs e)
     {
-        List<HackerNewsPostModel> newsItems = await FetchNewsAsync();
+        newsItems = await FetchNewsAsync();
         NewsCollectionView.ItemsSource = newsItems;
-    }
+    }*/
 
-    private async Task<List<HackerNewsPostModel>> FetchNewsAsync()
+    private async Task<ObservableCollection<HackerNewsPostModel>> FetchNewsAsync()
     {
         try
         {
             using (HttpClient client = new HttpClient())
             {
-                var c = 0;
-                List<HackerNewsPostModel> newsItems = new List<HackerNewsPostModel>();
-                HttpResponseMessage askPostIds = await client.GetAsync(AskStories);
+                ObservableCollection<HackerNewsPostModel> newsItems = new ObservableCollection<HackerNewsPostModel>();
 
-                string askItems = await askPostIds.Content.ReadAsStringAsync();
-                var askItemsTrimmed = askItems.TrimStart('[').TrimEnd(']').Split(',');
-                foreach (var item in askItemsTrimmed)
+                //foreach (var item in askItemsTrimmed)
+                for(int i = postCounter; i < 9; i++)
                 {
-                    HttpResponseMessage response = await client.GetAsync(ApiUrl + item + ".json");
+                    HttpResponseMessage response = await client.GetAsync(ApiUrl + askItemsTrimmed[i] + ".json");
                     var jsonItem = await response.Content.ReadFromJsonAsync<HackerNewsPostModel>();
                     if (string.IsNullOrEmpty(jsonItem.Text))
                     {
@@ -55,28 +59,53 @@ public partial class HackerNewsAskPage : ContentPage
                     
                     if(!jsonItem.Deleted){
                         newsItems.Add(jsonItem);
-                        c++;
-                    }
-
-                    if (c > 5)
-                    {
-                        break;
+                        postCounter++;
                     }
                 }
+                Debug.WriteLine("Number of posts BEFORE deletíng: " + askItemsTrimmed.Count());
+                askItemsTrimmed.RemoveRange(0, postCounter);
+                Debug.WriteLine("Number of posts AFTER deletíng: " + askItemsTrimmed.Count());
+                postCounter = 0;
                 return newsItems;
             }
         }
         catch (Exception ex)
         {
             Debug.WriteLine($"Error: {ex.Message}");
-            return new List<HackerNewsPostModel>();
+            return new ObservableCollection<HackerNewsPostModel>();
+        }
+    }
+
+    private async Task GetPostItems()
+    {
+        try
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage askPostIds = await client.GetAsync(AskStories);
+                string askItems = await askPostIds.Content.ReadAsStringAsync();
+                askItemsTrimmed = askItems.TrimStart('[').TrimEnd(']').Split(',').ToList();
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error: {ex.Message}");
         }
     }
 
     private async void OpenSelectedPost_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        Debug.WriteLine("Ask HN post clicked");
         HackerNewsPostModel post = (e.CurrentSelection.FirstOrDefault() as HackerNewsPostModel);
         await Navigation.PushAsync(new PostContentPage(post));
+    }
+
+    private async void NewsCollectionView_RemainingItemsThresholdReached(object sender, EventArgs e)
+    {
+        var moreNewsItems = await FetchNewsAsync();
+        foreach (var item in moreNewsItems)
+        {
+            newsItems.Add(item);
+            Debug.WriteLine("#### Should have gotten new posts now ####");
+        }
     }
 }
